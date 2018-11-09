baeef8319546bd5db4c724a0bb335a46fd2eb665
JoJo <apple@apples-MacBook-Pro.local>
2018-11-08 17:45:33 -0800

    Mentoring session changes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ let gameOver = false;
 //Add event listener for the cells
 cells.forEach(function (cell) {
     cell.addEventListener('click', cellsClicked);
-    cell.addEventListener('click', checkPlayersTurn);
 });
 
 
@@ -24,30 +23,59 @@ let winningCombos = [
 
 // Alternate between players
 function cellsClicked(e) {
-    if (gameOver == false) {
-        if (playersTurn % 2 == 0) {
-            e.target.innerHTML = ' X ';
-        } else {
-            e.target.innerHTML = ' 0 ';
-        }
+    if (gameOver == true) {
+        clearBoard();
+        return;
     }
+    if (playersTurn % 2 == 0) {
+        e.target.textContent = ' X ';
+    } else {
+        e.target.textContent = ' 0 ';
+    }
+    //Run the function to check who turn it is every click
+    checkPlayersTurn();
+    //Run function to check if their is a winner every click
+    checkWinner();
 }
+
+
+
 
 function checkWinner() {
     for (var i = 0; i < winningCombos.length; i++) {
-        for (var j = 0; j < winningCombos.length; j++) {
-            console.log(winningCombos[i][j]);
+        let xsum = 0;
+        let osum = 0;
+        // Copy of winning combos that we are iterating through
+        let winIndex = winningCombos[i];
+        for (var j = 0; j < winIndex.length; j++) {
+            if(cells[winIndex[j]].textContent == ' X '){
+                xsum++;
+            } 
+            if(cells[winIndex[j]].textContent == ' 0 '){
+                    osum++;
+
+            }
         }
+
+            if(xsum == 3){
+                messageBox.textContent = "X is the Winner!";
+                gameOver = true;
+            } else if(osum == 3){
+                messageBox.textContent = "0 is the Winner!";
+                gameOver = true;
+            }
     }
 }
+
 
 
 // Add increments of 1 to playersTurn variable to determine who's turn it is
 // Once there are 9 clicks, alert that it's a tie
-function checkPlayersTurn(e) {
+function checkPlayersTurn() {
     playersTurn++;
     if (playersTurn == 9) {
         messageBox.textContent = "It's a tie!";
+        gameOver = true;
     }
 }
 
@@ -61,9 +89,10 @@ resetButton.forEach(function (reset) {
 function clearBoard() {
     for (i = 0; i < cells.length; i++) {
         cells[i].textContent = '';
-        messageBox.textContent = "";
-        playersTurn = 0
     }
+    messageBox.textContent = "";
+    playersTurn = 0;
+    gameOver = false;
 }
 
-checkWinner();
+
